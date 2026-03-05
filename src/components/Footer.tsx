3e2dcc5d250452ff23eb33abc2a0f62f12c86451
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-05 23:34:50 +0000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,7 @@ const Footer = () => (
         <img
           src="/logo_fundo_transparente.png"
           alt="Rodrigo Póvoa Logo"
-          className="h-8 w-auto opacity-90 transition-all duration-300 
-                     drop-shadow-[0_0_8px_rgba(59,130,246,0.25)]
-                     hover:drop-shadow-[0_0_14px_rgba(59,130,246,0.45)]"
+          className="h-16 md:h-20 w-auto opacity-90 drop-shadow-[0_0_15px_rgba(59,130,246,0.5)]"
         />
       </div>
 
