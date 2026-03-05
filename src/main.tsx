26f927d4e7e9282f8e78d4b255956eaa53bef8c4
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-05 22:50:39 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
+
+<HelmetProvider>
+  <App />
+</HelmetProvider>
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
