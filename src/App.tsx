7e5860ae9b56d62299978836145ea6c45b152a9f
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-05 23:12:35 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Personal from "./pages/Personal";
 import Professional from "./pages/Professional";
+import Contact from "@/pages/Contact";
+<Route path="/contact" element={<Contact />} />
 
 import SideProjects from "./pages/SideProjects";
 
