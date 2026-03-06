064c6e3fc4611d0b3da0de2a1064d1ba488e4ae7
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-06 21:01:21 +0000

    Create PersonSchema.tsx

---
+++ src/components/seo/PersonSchema.tsx
@@ -0,0 +1,36 @@
+import { Helmet } from "react-helmet-async";
+
+export const PersonSchema = () => (
+  <Helmet>
+    <script type="application/ld+json">
+      {JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Person",
+        name: "Rodrigo Póvoa",
+        jobTitle: "Data Analytics Engineer & Team Leader",
+        url: "https://rodrigopovoa.com",
+        sameAs: [
+          "https://linkedin.com/in/rodrigocspovoa",
+          "https://github.com/rodcapella"
+        ],
+        knowsAbout: [
+          "Data Architecture",
+          "Azure",
+          "Data Engineer",
+          "Data Analytics",
+          "Team Leader",
+          "Tech Leader",
+          "Data Teams",
+          "Databricks",
+          "Power BI",
+          "Tableau",
+          "SQL",
+          "Python",
+          "PySpark",
+          "AI Systems",
+          "Data Governance"
+        ]
+      })}
+    </script>
+  </Helmet>
+);
