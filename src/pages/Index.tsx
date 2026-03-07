8985cd7cdd9670956379454cc75b0c0ec05565bf
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-07 17:45:56 -0500

    Fix: Add missing BreadcrumbSchema import in Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import { Helmet } from "react-helmet-async";
+import { BreadcrumbSchema } from "@/components/seo/BreadcrumbSchema";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ExpertiseSection from "@/components/ExpertiseSection";
