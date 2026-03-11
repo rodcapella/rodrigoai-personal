1af6bf7b4b49f56ec84312fdbe1a9fa09c3bc9cd
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-11 14:06:06 +0000

    update sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({
-      hostname: "https://www.rpovoadata.tech",
-      dynamicRoutes: [
-        "/",
-        "/professional",
-        "/personal",
-        "/why-me",
-        "/side-projects",
-        "/contact"
-      ]
-    })
   ],
 
   resolve: {
