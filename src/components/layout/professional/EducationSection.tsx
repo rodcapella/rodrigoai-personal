f561a51bb0a6d544878006bb0fd68c42cd45f595
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-13 13:17:07 +0000

    create EducationSector

---
+++ src/components/layout/professional/EducationSection.tsx
@@ -0,0 +1,12 @@
+import Section from "@/components/layout/Section"
+import SectionTitle from "@/components/layout/SectionTitle"
+
+const EducationSection = () => {
+  return (
+    <Section>
+      <SectionTitle>Academic Background</SectionTitle>
+    </Section>
+  )
+}
+
+export default EducationSection
