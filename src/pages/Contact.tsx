5ce9081a2fdad96d481312a2c489ad1d7d3931be
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-18 19:39:21 +0000

    fix errors

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -215,7 +215,6 @@ export default function Contact({ theme, onToggleTheme }: any) {
               </motion.form>
             )}
           </AnimatePresence>
-        </div>
       </PageSection>
     </MainLayout>
   );
